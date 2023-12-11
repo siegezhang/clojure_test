6271d4f492bc482d7a3394b19b01f140c4dbcbc8
xiaosheng.zhang <z1178858918@qq.com>
2023-12-11 10:12:05 +0800

    commit

--- src/clojure/examples/hello.clj
+++ src/clojure/examples/hello.clj
@@ -3,4 +3,40 @@
 
 (defn -main
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (println (= (list :a :b :c
+                    ) '(:a :b :c))
+           )
+  (def user-data {:name "John Doe" :age 30})
+  (println user-data)
+
+  (println (= [:a :b :c] (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c)))
+  (println (= [:a :b :c] (list :a :b :c)))
+
+  (= 8 ((fn add-five [x] (+ x 5)) 3))
+  (= 8 ((fn [x] (+ x 5)) 3))
+  (= 8 (#(+ % 5) 3))
+  (= 8 ((partial + 5) 3))
+
+  (println (= '(6 7 8) (map #(+ % 5) '(1 2 3))))
+  (println (= '(6 7) (filter #(> % 5) '(3 4 5 6 7))))
+
+  (println (= ((fn a [l] (nth l (- (count l) 1))) [1 2 3 4 5]) 5))
+  (= ((fn a [l] (nth l (+ (count l) -2))) (list 1 2 3 4 5)) 4)
+
+  ;(= (__ [1 2 3 4 5]) 5)
+  ;(= (__ '(5 4 3)) 3)
+  ;(= (__ ["b" "c" "d"]) "d")
+  (let [[head & tail] '(1 2 3)] (println head) (println tail))
+
+  (println ((defn get-nth [seq n] (first (drop n seq))) '(4 5 6 7) 2))
+  (println (= (#(reduce + (map (fn [x] 1) %)) '(1 2 3 3 1)) 5))
+
+  reduce #(cons %2 %1) '()
+
+
+  )
+
+
+
+
