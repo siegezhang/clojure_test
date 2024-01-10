b4609fbdbb85cacd42d5f49bcaa94aadf7e425d7
xiaosheng.zhang <z1178858918@qq.com>
2024-01-10 15:42:58 +0800

    commit

--- src/clojure_test/basic/basic_test.clj
+++ src/clojure_test/basic/basic_test.clj
@@ -23,12 +23,13 @@
 (is (= :empty-but-truthy (if [] :empty-but-truthy :empty-and-falsey)))
 
 
-(println (clojure.string/replace "This is _simple_ markup."
-                                 #"_([^_]+)_"
-                                 (fn [[_ s]]
-                                   (str "<strong>"
-                                        (clojure.string/upper-case s)
-                                        "</strong>"))))
+(println
+ (clojure.string/replace "This is _simple_ markup."
+                         #"_([^_]+)_"
+                         (fn [[_ s]]
+                           (str "<strong>"
+                                (clojure.string/upper-case s)
+                                "</strong>"))))
 
 (println (remove pos? [1 -2 2 -1 3 7 0]))
 
@@ -38,34 +39,44 @@
 
 (remove even? (range 10))
 
-(remove (fn [x]
-          (= (count x) 1))
-        ["a" "aa" "b" "n" "f" "lisp" "clojure" "q" ""])
+(remove
+ (fn [x]
+   (= (count x) 1))
+ ["a" "aa" "b" "n" "f" "lisp" "clojure" "q" ""])
 
 
 (defn re-map [re f s]
   (remove #{::padding}
           (interleave
-            (clojure.string/split s re)
-            (concat (map f (re-seq re s)) [::padding]))))
+           (clojure.string/split s re)
+           (concat (map f (re-seq re s)) [::padding]))))
 
 
 (defn re-map1 [re f s]
   (interleave
-    (clojure.string/split s re)
-    (concat (map f (re-seq re s)) [::padding])))
+   (clojure.string/split s re)
+   (concat (map f (re-seq re s)) [::padding])))
 
 
-(println (re-map #"_([^_]+)_"
-                 (fn [[_ s]]
-                   [:strong s])
-                 "This is _simple_ markup."))
+(println
+ (re-map #"_([^_]+)_"
+         (fn [[_ s]]
+           [:strong s])
+         "This is _simple_ markup."))
 
 (defn re-map [re f s]
   (if (re-matches re s)
     [(f s)]
     (remove #{"" ::padding}
             (interleave
-              (clojure.string/split s re)
-              (concat (map f (re-seq re s)) [::padding])))))
+             (clojure.string/split s re)
+             (concat (map f (re-seq re s)) [::padding])))))
+
 (re-map #"hello" (constantly :match) "hello")
+
+(let [id 1]
+  (let [id 2]
+    (println id))
+  (println id))
+
+
