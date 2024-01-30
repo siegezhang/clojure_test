7e267d08aaa5c85d335fb99f53245f2a398bfa88
xiaosheng.zhang <z1178858918@qq.com>
2024-01-30 19:57:18 +0800

    commit

--- src/clojure_test/collections/seq_test.clj
+++ src/clojure_test/collections/seq_test.clj
@@ -18,8 +18,9 @@
 ;; When we don't need the destructured symbol we can
 ;; use the underscore to indicate this. But any name will do.
 (let [[username _ city] items]
-  (is (= "mrhaki lives in Tilburg"
-         (str username " lives in " city))))
+  (is
+   (= "mrhaki lives in Tilburg"
+      (str username " lives in " city))))
 
 ;; We can destructure sequences just like vectors.
 (def coords '(29.20090, 12.90391))
@@ -65,5 +66,71 @@
   [[first second :as all]]
   (str first ", " second " and " (- (count all) 2) " more fruit names."))
 
-(is (= "Apple, Pear and 3 more fruit names."
-       (summary basket)))
+(is
+ (= "Apple, Pear and 3 more fruit names."
+    (summary basket)))
+
+;; Notice that y is iterated over for every x. x could be seen as the 'outer' loop
+(doseq [x [-1 0 1]
+        y [1 2 3]]
+  (prn (* x y)))
+
+(doseq [[x y] (map list [1 2 3] [1 2])]
+  (prn (* x y)))
+
+(map list [1 2 3] [1 2 3])
+
+
+(doseq [[[_ b] [_ d]] (map list (sorted-map :1 1 :2 2) (sorted-map :3 3 :4 4))]
+  (prn (* b d)))
+
+
+(doseq [[k v] (map identity {:1 1 :2 2 :3 3})]
+  (prn k v))
+
+;;等价于
+
+(doseq [[k v] {:1 1 :2 2 :3 3}]
+  (prn k v))
+
+(doseq [a [1 2]
+        b [3 4]]
+  (println a b))
+
+(doseq [x     (range 6)
+        :when (odd? x)
+        :let  [y (* x x)]]
+  (println [x y]))
+
+
+(doseq [x      (range 99)
+        :let   [y (* x x)]
+        :while (< y 30)]
+  (println [x y]))
+
+
+(for [x     (range 5)
+      :when (> (* x x) 3)]
+  (* 2 x))
+
+(def v (vec (range 1 65)))
+(def m (map #(do (prn %1) (str %1 "!")) v))
+
+(println (first m))
+
+;;(range 1 65)没有赋值给变量,不能惰性求值
+(def v (vec (range 1 65)))
+(def m (map #(do (prn %1) (str %1 "!")) v))
+
+(first m)
+
+;; prints 1-32, followed by "1!"
+(nth m 10)
+
+;; prints "11!"
+
+;;A large range is of type LongRange whereas an infinite range, which you can
+;create with (range), is of type Iterate and is an iterator that yields values (similar to a
+;generator in other programming languages, like Python).
+;;赋值给变量,可以惰性求值
+(def r (range 1 999999999))
